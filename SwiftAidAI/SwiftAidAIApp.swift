89f07795a1ce5f231aad107d434fea28d3f4f5f9
nafisahi <91844297+nafisahi@users.noreply.github.com>
2025-03-12 15:39:47 +0000

    Setup initial Xcode project and added necessary files

---
+++ SwiftAidAI/SwiftAidAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAidAIApp.swift
+//  SwiftAidAI
+//
+//  Created by Nafisah Islam on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAidAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
